b16de48ca5720fcdd9db81bc3eb01cceb98911e6
Maggie Ward <mlward639@gmail.com>
2021-06-26 18:07:33 -0400

    fetch for 5day forecast API and fetch for OneCall weather API

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,26 +1,51 @@
 //api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-/* try again in a couple hrs to see if API key is working
-var baseUrl = 'api.openweathermap.org/data/2.5/weather'
+//try again in a couple hrs to see if API key is working
+//var baseUrl = 'https://api.openweathermap.org/data/2.5/weather'
+
+
 var APIKey = '3a1df45ecc477b266d4c9b728ee5cd1e'
 
 var cityName = 'Atlanta' //update later to be what they enter into the form
+/*
+coord
+coord.lon City geo location, longitude
+coord.lat City geo location, latitude
+*/
 
-function getAPI() {
+function getOneCallWeatherAPI() {
+    var baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
     var requestUrl = baseUrl + '?q=' + cityName + '&appid=' + APIKey;
-    console.log(requestUrl)
+
+    console.log("requestURL: "+ requestUrl)
     fetch(requestUrl)
         .then(function (response) {
             return response.json();
         })
         .then(function(data) {
+            console.log("one call weather data next line")
             console.log(data);
         })
 
 
 }
 
-getAPI()
-*/
+getOneCallWeatherAPI()
+
+function getFiveDayForecastAPI() {
+    var baseUrl = 'https://api.openweathermap.org/data/2.5/forecast';
+    var requestUrl = baseUrl + '?q=' + cityName + '&appid=' + APIKey;
+    console.log("requestURL: " + requestUrl);
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log("Five day forcast data next line")
+            console.log(data)
+        })
+}
+getFiveDayForecastAPI();
+/*
 var searchBtn = document.querySelector('.search-btn')
 var generatedCitiesUl = document.querySelector('.generated-cities-ul')
 
@@ -62,3 +87,4 @@ searchBtn.addEventListener('click', function creatingCityLists (event){
     return;
 }
 )
+*/
