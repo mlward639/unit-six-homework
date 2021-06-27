e9921aec1a6697b3be370c89638f655ae7b99c8d
Maggie Ward <mlward639@gmail.com>
2021-06-27 02:21:54 -0400

    main city/current weather section working minus the icon

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,7 +3,20 @@
 var blankInputEl = document.querySelector("#input-city-name");
 var searchBtn = document.querySelector('.search-btn')
 var generatedCitiesUl = document.querySelector('.generated-cities-ul')
+var currentCityH2 = document.querySelector('.current-city-name')
+var currentTemp = document.querySelector('.current-temp');
+var currentWind = document.querySelector('.current-wind');
+var currentHumidity = document.querySelector('.current-humidity');
+var currentUVI = document.querySelector('.current-UVI')
+var weatherIconCurrent = document.querySelector(".weather-icon-current")
+var weatherIconCard1 = document.querySelector(".weather-icon-card1")
+var weatherIconCard2 = document.querySelector(".weather-icon-card2")
+var weatherIconCard3 = document.querySelector(".weather-icon-card3")
+var weatherIconCard4 = document.querySelector(".weather-icon-card4")
+var weatherIconCard5 = document.querySelector(".weather-icon-card5")
+var UVIBtn = document.createElement("BUTTON")
 
+//for moving weather from current to one of the 5 cards, see if you can reassign the class from the current container to the card to move that information down...
 var APIKey = '3a1df45ecc477b266d4c9b728ee5cd1e'
 
 var inputedCityName = '' //update later to be what they enter into the form
@@ -16,13 +29,8 @@ var combinedSavedCityArray
 var error = new Error('Cannot search the same city twice');
 var lat;
 var lon;
-
-/* ONE CALL HAS
-CURRENT WEATHER, COORDs
-coord
-coord.lon City geo location, longitude
-coord.lat City geo location, latitude
-*/
+ 
+var today = moment().format("MM/DD/YY");
 
 function getInputValue () {
     inputedCityName = blankInputEl.value;
@@ -51,6 +59,17 @@ function getCurrentWeatherAPI() {
             console.log("lat : " + lat);  
             lon = data.coord.lon;
             console.log("lon: " + lon);
+            console.log("icon " + data.weather[0].icon)
+            if (data.weather[0].icon == "01d" || "01n") {
+                weatherIconCurrent.src='\images\01d.png' //why wont it link to my picture??? says 404 not found for the image
+            }
+            currentCityH2.textContent = inputedCityName + " (" + today + ")";
+            console.log("temp: " + data.main.temp);
+            currentTemp.textContent = "Temp: " + data.main.temp + "\u00B0 F";
+            console.log("wind: " + data.wind.speed);
+            currentWind.textContent = "Wind: " + data.wind.speed + " MPH";
+            console.log("humidity: " + data.main.humidity);
+            currentHumidity.textContent = "Humidity: " + data.main.humidity + "%";
             function getOneCallWeatherAPI() {
                 var baseUrl = 'https://api.openweathermap.org/data/2.5/onecall';  
                 var requestUrl = baseUrl + '?lat=' + lat + '&lon=' + lon + '&exclude=minutely,hourly,alerts&units=imperial&appid=' + APIKey
@@ -62,9 +81,22 @@ function getCurrentWeatherAPI() {
                     .then(function(data) {
                         console.log("OneCall data next line")
                         console.log(data)
+                        UVIBtn.textContent = data.current.uvi //make sure it is matching the unique UVI of each city when generated each time and not duplicating button
+                        currentUVI.appendChild(UVIBtn);
+                        console.log("UV index: " + data.current.uvi);
+                        if (data.current.uvi == 0 || data.current.uvi == 1 || data.current.uvi == 2) {
+                            UVIBtn.style.backgroundColor = "green";
+                        } else if (data.current.uvi == 3 || data.current.uvi == 4 || data.current.uvi == 5) {
+                            UVIBtn.style.backgroundColor = "yellow";
+                        } else if (data.current.uvi == 6 || data.current.uvi == 7) {
+                            UVIBtn.style.backgroundColor = "orange";
+                        } else if (data.current.uvi == 8 || data.current.uvi == 9 || data.current.uvi == 10) {
+                            UVIBtn.style.backgroundColor = "red";
+                        } else if (data.current.uvi > 10) {
+                            UVIBtn.style.backgroundColor = "purple";
+                        }
                     })
             }
-
             getOneCallWeatherAPI();  
         })
 }
